1bb67659ce7edcce990bc05da4ce554a19186e9b
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2022-02-17 19:22:51 -0300

    lombok: val

---
+++ src/main/java/features/FeatureVal.java
@@ -0,0 +1,19 @@
+package features;
+
+import lombok.val;
+
+import java.util.ArrayList;
+
+public class FeatureVal {
+  public void show() {
+    val messages = new ArrayList<>();
+
+    // Not modified after initialize
+    // message = "";
+
+    // Usages
+    for (val message : messages) {
+      System.out.println(message);
+    }
+  }
+}
